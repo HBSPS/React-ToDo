dae48dea5f3afb6b9b1ea77d02b675d01b367dab
HBSPS <wiyeong79@naver.com>
2022-08-16 22:41:49 +0900

    :sparkles: Input Validation
    Input Validation without React-Hook-Form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 
 function ToDoList() {
     const [toDo, setToDo] = useState("");
+    const [toDoError, setToDoError] = useState("");
     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
         const { currentTarget: { value } } = event;
+        setToDoError("");
         setToDo(value);
     };
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(toDo);
+        if (toDo.length < 11) {
+            return setToDoError("To do should be longer than 10")
+        };
+        console.log("submit");
     };
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input onChange={onChange} value={toDo} placeholder="Write what you will do" />
                 <button>Add</button>
+                {toDoError !== "" ? toDoError : null}
             </form>
         </div>
     );
