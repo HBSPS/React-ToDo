ff25886f12609623e62ec45a74610e5177da14c2
HBSPS <wiyeong79@naver.com>
2022-08-18 22:39:18 +0900

    :art: Refactoring Code
    Refactoring Code

---
+++ src/components/CreateToDo.tsx
@@ -0,0 +1,24 @@
+import { useForm } from "react-hook-form";
+import { useSetRecoilState } from "recoil";
+import { toDoState } from "../atoms";
+
+interface IForm {
+    toDo: string;
+};
+
+function CreateToDo() {
+    const setToDos = useSetRecoilState(toDoState);
+    const { register, handleSubmit, setValue } = useForm<IForm>();
+    const handleValid = ({toDo}: IForm) => {
+        setToDos((prev) => [{text: toDo, id: Date.now(), category: "TO_DO"}, ...prev]);
+        setValue("toDo", "");
+    };
+    return (
+        <form onSubmit={handleSubmit(handleValid)}>
+            <input {...register("toDo", {required: "Please write a To Do"})} placeholder="Write what you will do" />
+            <button>Add</button>
+        </form>
+    );
+};
+
+export default CreateToDo;
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,38 +1,17 @@
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-
-interface IToDo {
-    text: string;
-    id: number;
-    category: "TO_DO" | "DOING" | "DONE";
-};
-
-const toDoState = atom<IToDo[]>({
-    key: "toDo",
-    default: []
-});
-
-interface IForm {
-    toDo: string;
-};
+import { useRecoilState, useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-    const [toDos, setToDos] = useRecoilState(toDoState); // [불러온 atom의 이름 지정, modifier function] = useRecoilState(불러올 atom)
-    const { register, handleSubmit, setValue } = useForm<IForm>();
-    const handleValid = ({toDo}: IForm) => {
-        setToDos((prev) => [{text: toDo, id: Date.now(), category: "TO_DO"}, ...prev]);
-        setValue("toDo", "");
-    };
+    const toDos = useRecoilValue(toDoState);
     return (
         <div>
             <h1>To Dos</h1>
             <hr />
-            <form onSubmit={handleSubmit(handleValid)}>
-                <input {...register("toDo", {required: "Please write a To Do"})} placeholder="Write what you will do" />
-                <button>Add</button>
-            </form>
+            <CreateToDo />
             <ul>
-                {toDos.map((toDo) => <li key={toDo.id}>{toDo.text}</li>)}
+                {toDos.map((toDo) => <ToDo key={toDo.id} {...toDo} />)}
             </ul>
         </div>
     );
